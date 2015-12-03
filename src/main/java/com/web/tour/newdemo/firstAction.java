c328b7c2328ee6d4f3d7100c8f7eae215633e6df
Monkey-mi <mishengliang@live.com>
2015-12-03 09:47:51 +0800

    旅游路线工程

---
+++ src/main/java/com/web/tour/newdemo/firstAction.java
@@ -0,0 +1,15 @@
+package com.web.tour.newdemo;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class firstAction extends ActionSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3793214886422525913L;
+
+	public String execute(){
+		return "success";
+	}
+}
---
+++ src/main/java/com/web/tour/newdemo02/secondDemo.java
@@ -0,0 +1,10 @@
+package com.web.tour.newdemo02;
+
+public class secondDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
